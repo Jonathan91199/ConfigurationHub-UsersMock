84071c7d413addf607bf0c7c57d3bc9960bc5f7e
Jonathan91199 <jonathan.c911@gmail.com>
2021-01-05 23:58:51 +0200

    connecting to mongo
    enabling cors
    handling user requests

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+let AddNewUser = require('./mongoDB/mongoActions/AddNewUser')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,11 +24,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.post('/users', function(req, res){
+  AddNewUser(req, res)
+})
+app.listen(8000)
+
+mongoose.connect('mongodb://localhost:27017/ConfigurationHub-UserMock', function(){
+  console.log("\x1b[36m", "* * * Connected To Mongo DataBase * * *")
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
